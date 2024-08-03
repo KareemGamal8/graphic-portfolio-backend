1e9b4e913d36493a3c9ba9015fc4e3696a35b546
kareemgmal7755 <kareemgmal7755@gmail.com>
2024-08-03 13:49:23 +0300

    Vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
 
 // Enable CORS for all routes
